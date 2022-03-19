f0e89c9178d786801e2e19373dc1b1e89a24471e
heewon <huiwon1488@gmail.com>
2022-03-19 23:51:49 +0900

    0319 study

--- c++/2798/2798.cpp
+++ c++/2798/2798.cpp
@@ -1,49 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int max_total = 0;      //숫자 3개의 합
-int* visited;            // 방문한 숫자 저장
-int N, M;               // 카드의 개수, 블랙잭 숫자
-int* cards;             // 카드 번호
-
-
-int blackjack(int k, int total) { //k 현재 카드의 개수, total 세 수의 합
-    // 만약 숫자의 합이 M을 넘어가면 return
-    if(total > M) {
-        return 0;
-    }
-    if(k == 3) {
-        if (abs(max_total-M) > abs(total-M)){
-            max_total = total;
-            return 0;
-        }
-    }
-    for(int i = 0; i<N; i++) {
-        // 현재 카드를 방문한 적이 없으면 해당 카드를 더하고 다음 수를 찾는다.
-        if (visited[i] == 0){
-            visited[i] = 1;
-            blackjack(k+1, total + cards[i]);
-            visited[i] = 0;
-        }
-    }
-    return 0;
-}
-
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N, M;
+    int total;
+    int max_num = 0;
+
     cin >> N >> M;
     
-    cards = new int[N];
-    visited =  new int[N]{};
+    int cards[N];
     
     for (int i = 0; i < N; i++){
         cin >> cards[i];
     }
 
-    blackjack(0, 0);
-
-    cout << max_total << '\n';
-    
-    delete visited, cards;
+    for(int i=0;i<N;i++){
+        for(int j=i+1;j<N;j++){
+            for(int k=j+1;k<N;k++){
+                total = cards[i] + cards[j] + cards[k];
+                if(total > M){
+                    continue;
+                }
+                if(max_num < total){
+                    max_num = total;
+                }
+            }
+        }
+    }
+        
+    cout << max_num;
     return 0;
 }
--- javascript/VanillaJS/momentum/app.js
+++ javascript/VanillaJS/momentum/app.js
@@ -41,3 +41,133 @@
 // };
 
 // console.log(player.name); // heewon
+
+
+// 2-7,8 function
+// function sayHello(nameOfPerson, age){
+//   console.log("Hello my name is " + nameOfPerson + " and I am " + age);
+// }
+
+// sayHello("heewon", 25);
+
+// function plus(a, b){
+//   console.log(a + b);
+// }
+// plus(10, 13);
+
+// const player = {
+//   name: "heewon",
+//   sayHello: function(personName){
+//     console.log("hello " + personName + " nice to meet you")
+//   },
+// }
+
+// console.log(player.name)
+// player.sayHello("Lea")
+
+// 2-9,10 recap
+// const a = 5; // 변경할 수 없는 변수
+// let b = 6; // 변경할 수 있는 변수
+// b = 7;
+
+// let a1 = true;  // bool
+// let a2 = "ddd"; // text
+// let a3 = null;  // nothing, 아직 할당되지 않음, 비어있음
+// let a4 = undefined; // 존재하긴 하지만 아예 없음.
+
+// let li = ['a', 'b', 'c']; // array
+// console.log(li[1]);
+// li.push("d");
+// console.log(li);
+
+// const player = {
+//   name: "heewon",
+//   age: 23,
+// };
+// player.name = "lea";
+// console.log(player);
+
+// function multiply(a, b){ // argument. place holder
+//   console.log(a * b);
+// }
+
+// multiply(2, 4);
+
+// const calculator = {
+//   plus: function(a, b){ console.log(a + b) },
+//   multiply: function(a, b){ console.log(a * b) },
+//   minus: function(a, b){ console.log(a - b) },
+//   devide: function(a, b){ console.log(a / b) },
+//   power: function(a, b){ console.log(a ** b) },
+// }
+
+// calculator.plus(1, 2);
+// calculator.multiply(1, 2);
+// calculator.minus(1, 2);
+// calculator.devide(1, 2);
+// calculator.power(1, 2);
+
+// 2-11,12
+// const age = 25;
+// function calculateAmAge(myAge) {
+//   return myAge -2;
+// }
+
+// const amAge = calculateAmAge(age);
+
+// console.log(amAge);
+
+// // 2-13
+// const age = prompt("How old are you?"); // 프롬포트 창으로 사용자에게 입력값을 받는다. 굉장히 오래된 방범.
+
+
+// //console.log(typeof age, typeof parseInt(age)); // typeof는 변수의 타입을 반환
+// console.log(age, parseInt(age)); // NaN는 'Not a Number'라는 의미
+// parseInt(); // parseInt("")문자 타입을 숫자 타입으로 변경
+
+// 2-14,15 conditionals
+// const age = parseInt(prompt("How old are you?")); // 프롬포트 창으로 사용자에게 입력값을 받는다. 굉장히 오래된 방범.
+
+// // isNaN(); // isNan()은 숫자인지 NaN인지 여부 확인
+
+// if(isNaN(age)){ //if 조건문, ()안의 조건문이 참일 때 실행
+//   console.log("Please write a number");
+// } else if (age < 18) {
+//   console.log("You are too young");
+// } else if (age >= 18 && age < 50) { // && : and ||: or
+//   console.log("You can drink.")
+// } else if (age >= 50 && age < 80){
+//     console.log("You are too old to drink.");
+// } else {
+//     console.log("You can do whatever you want.");
+// }
+
+// if (age === 101){
+//   console.log("you are good");
+// } else if (age !== 101){
+//   console.log("you are also good");
+// }
+
+// 3-1
+
+// document //html을 자바스크립트 관점에서 볼 수 있음. html에서 작성된 부분을 자바스크립트에서 받아서 사용할 수 있음
+// console.log(document.title)
+// document.title = "Hi";
+// document.body;
+
+// // 3-2
+// const title = document.getElementById("title");
+// console.dir(title);
+
+// title.innerText = "Hello!";
+
+// console.log(title.id);
+// console.log(title.className); // Html의 element를 가져올 수 있음.
+
+// 3-3
+
+const Hellos = document.getElementsByClassName("hello");
+console.log(Hellos);
+
+const title = document.querySelector(".hello hi"); // selector
+const titles = document.querySelectorAll(".hello hi"); // selector
