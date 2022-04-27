ebdc02c5d7c9e4c9bb865022e372a9b72144c4e9
heewon <huiwon1488@gmail.com>
2022-04-27 23:51:07 +0900

    0427 study

--- 이코테/DP/개미 전사/3.py
+++ 이코테/DP/개미 전사/3.py
@@ -1,8 +1,18 @@
-
 # 만약 현재 창고 번호가 i일 때,
-# i-1 번째까지의 합보다
-# i-2번째와 i번째 창고의 식량이 더 많을때 현재 식량창고를 턴다
+# i-1 번째까지의 누적합보다
+# i-2번째와 i번째 창고의 식량이 더 많은 것을 골라 턴다
+
+import sys
+sys.stdin = open('input.txt', 'r')
 
 N = int(input())
-dp = list(map(int, input().split()))
+foods = list(map(int, input().split()))
+dp = [0] * N
 
+dp[0] = foods[0]
+dp[1] = max(foods[0], foods[1])
+
+for i in range(2, N):
+    dp[i] = max(dp[i-2] + foods[i], dp[i-1])
+
+print(dp[-1])
